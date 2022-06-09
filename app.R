226c01a01c6add52206b588170dde967da857a0e
Ammlakh <mrinal.lakhotia@gmail.com>
2022-06-09 13:10:16 -0700

    app update for 3 functions

--- app.R
+++ app.R
@@ -1,42 +1,55 @@
 ####################################
-# Data Professor                   #
-# http://youtube.com/dataprofessor #
-# http://github.com/dataprofessor  #
+# Anmol Lakhotia
 ####################################
-
-# Modified from Winston Chang, 
-# https://shiny.rstudio.com/gallery/shiny-theme-selector.html
-
-# Concepts about Reactive programming used by Shiny, 
-# https://shiny.rstudio.com/articles/reactivity-overview.html
 
 # Load R packages
 library(shiny)
 library(shinythemes)
+library(CFBUS342)
 
 
   # Define UI
   ui <- fluidPage(theme = shinytheme("yeti"),
     navbarPage(
-      "My first app",
-      tabPanel("Navbar 1",
+      "BUS 342",
+      tabPanel("Present Value Calculator",
                sidebarPanel(
                  tags$h3("Input:"),
-                 textInput("txt1", "Given Name:", ""),
-                 textInput("txt2", "Surname:", ""),
-                 
-               ), # sidebarPanel
-               mainPanel(
-                            h1("Header 1"),
-                            
-                            h4("Output 1"),
-                            verbatimTextOutput("txtout"),
-
-               ) # mainPanel
+                 tags$h5("Future Value"),
+                 textInput("pv.fv", ""),
+                 tags$h5("Periods"),
+                 textInput("pv.periods", ""),
+                 tags$h5("Interest Rate"),
+                 textInput("pv.interest_rate", ""),), # sidebarPanel
+               mainPanel(h1("Output"), h4("Present Value"), verbatimTextOutput("pv.txtout")) # mainPanel
                
       ), # Navbar 1, tabPanel
-      tabPanel("Navbar 2", "This panel is intentionally left blank"),
-      tabPanel("Navbar 3", "This panel is intentionally left blank")
+      
+      tabPanel("Future Value Calculator",
+               sidebarPanel(
+                 tags$h3("Input:"),
+                 tags$h5("Present Value"),
+                 textInput("fv.pv", ""),
+                 tags$h5("Periods"),
+                 textInput("fv.periods", ""),
+                 tags$h5("Interest Rate"),
+                 textInput("fv.interest_rate", ""),), # sidebarPanel
+               mainPanel(h1("Output"), h4("Future Value"), verbatimTextOutput("fv.txtout")) # mainPanel
+               
+      ), # Navbar 2, tabPanel
+      tabPanel("Perpetuity Present Value Calculator",
+               sidebarPanel(
+                 tags$h3("Input:"),
+                 tags$h5("Payment"),
+                 textInput("perpetuity.payment", ""),
+                 tags$h5("Discount Rate"),
+                 textInput("perpetuity.discount_rate", ""),
+                 tags$h5("Growth Rate"),
+                 textInput("perpetuity.growth_rate", ""),), # sidebarPanel
+               mainPanel(h1("Output"), h4("Present Value"), verbatimTextOutput("perpetuity.txtout")) # mainPanel
+               
+      ) # Navbar 3, tabPanel
+      
   
     ) # navbarPage
   ) # fluidPage
@@ -45,8 +58,16 @@ library(shinythemes)
   # Define server function  
   server <- function(input, output) {
     
-    output$txtout <- renderText({
-      paste( input$txt1, input$txt2, sep = " " )
+    output$pv.txtout <- renderText({
+      PV(as.numeric(input$pv.fv), as.numeric(input$pv.periods), as.numeric(input$pv.interest_rate))
+    })
+    
+    output$fv.txtout <- renderText({
+      FV(as.numeric(input$fv.pv), as.numeric(input$fv.periods), as.numeric(input$fv.interest_rate))
+    })
+    
+    output$perpetuity.txtout <- renderText({
+      PERPETIUTY_PV(as.numeric(input$perpetuity.payment), as.numeric(input$perpetuity.discount_rate), as.numeric(input$perpetuity.growth_rate))
     })
   } # server
   
